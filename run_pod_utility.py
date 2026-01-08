a7d4b4c0d100ec84f4a0aea3bbb9f47d98b3f847
Aashish-0011 <aashish@twebsolutions.com>
2026-01-08 13:11:08 +0530

    update the code to  handle  mutiple gpu

--- run_pod_utility.py
+++ run_pod_utility.py
@@ -27,21 +27,56 @@ def get_running_pod():
         for pod in running_pods:
             print('running pod:', pod)
             if (pod.get('imageName') == 'nextdiffusionai/comfyui-sageattention:cuda12.8') or (pod.get('imageName') == 'nextdiffusionai/comfyui-sageattention:cuda12.8-new'):
+                gpuCount=pod.get('gpuCount')
                 print('Found running pod with imageName nextdiffusionai/comfyui-sageattention:cuda12.8')
                 print('Pod ID:', pod.get('id'))
                 logger.info(f'Running pod id fetched {pod.get('id')}')
-                return pod.get('id')
+                return pod.get('id'), gpuCount
             
         
-        return None
+        return None, None
     except Exception as e:
         logger.error(f"Unable to  fetch the runing  pof id  due to {str(e)}")
+        return None, None
+
+
+#  finctioon to  map with ip , port and worflow
+def map_ip_workflow(gpu_count: int = 1, RUNPOD_ID: str= None):
+    INITIAL_PORT=8188
+    WORKFLOW_IP={}
+    RUNPOD_ID=str(RUNPOD_ID)
+
+    if gpu_count == 1:
+       WORKFLOW_IP= {
+           't2i': f"https://{RUNPOD_ID}-8188.proxy.runpod.net",
+           't2v': f"https://{RUNPOD_ID}-8188.proxy.runpod.net",
+           'i2v': f"https://{RUNPOD_ID}-8188.proxy.runpod.net",
+        }
+    
+    elif gpu_count ==2:
+        WORKFLOW_IP= {
+           't2i': f"https://{RUNPOD_ID}-8188.proxy.runpod.net",
+           't2v': f"https://{RUNPOD_ID}-8188.proxy.runpod.net",
+           'i2v': f"https://{RUNPOD_ID}-8189.proxy.runpod.net",
+        }
+    
+    else:
+        WORKFLOW_IP= {
+           't2i': f"https://{RUNPOD_ID}-8188.proxy.runpod.net",
+           't2v': f"https://{RUNPOD_ID}-8189.proxy.runpod.net",
+           'i2v': f"https://{RUNPOD_ID}-8190.proxy.runpod.net",
+        }
+    return WORKFLOW_IP
+
 
 
 if __name__ == "__main__":
-    running_pod_id=get_running_pod()
-
-
+    running_pod_id, gpuCount=get_running_pod()
 
     print('pods:', running_pod_id)
+    print('gpuCount:', gpuCount)
+
+    comfy_url=map_ip_workflow(gpu_count=gpuCount, RUNPOD_ID=running_pod_id)
+
+    print('comfy_url--->>>',comfy_url)
 
